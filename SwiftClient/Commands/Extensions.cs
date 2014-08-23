0d2e666ffb9ecde0964a55116cc614d17acd9d61
Paul Cunningham <pjcunningham@borsuk.co.uk>
2014-08-23 23:25:38 +0100

    Linq chunking

---
+++ SwiftClient/Commands/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwiftClient.Commands {
+    public static class Extensions {
+
+        public static IEnumerable<IEnumerable<TSource>> ChunkData<TSource>(this IEnumerable<TSource> source, int chunkSize) {
+            for (int i = 0; i < source.Count(); i += chunkSize)
+                yield return source.Skip(i).Take(chunkSize);
+        } 
+    }
+}
